f459062cf49780011c0f06a56b32cf3c58db2587
Ben Nuttall <ben@bennuttall.com>
2013-06-16 01:12:51 +0100

    Added twitter badges and mailchimp subscribe count

--- footer-content.php
+++ footer-content.php
@@ -1,2 +1,6 @@
+<a href="https://twitter.com/ben_nuttall" class="twitter-follow-button" data-show-count="false">Follow @ben_nuttall</a>
+<script type="text/javascript" language="JavaScript" src="http://piweekly.us7.list-manage.com/subscriber-count?b=11&u=50a341f5-6390-4e2a-a315-b2ab55cb32f5&id=2141506785"></script>
+<a href="https://twitter.com/ryanteck" class="twitter-follow-button" data-show-count="false">Follow @ryanteck</a><br /><br />
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 piweekly.net is a community project by <a href="http://twitter.com/ben_nuttall">Ben Nuttall</a> and <a href="http://twitter.com/ryanteck">Ryan Walmsley</a>.<br />
 <a href="https://github.com/bennuttall/piweekly"><em>piweekly</em></a> Wordpress Theme by <a href="http://bennuttall.com/">Ben Nuttall</a> - source available on <a href="https://github.com/bennuttall/piweekly">github</a>.
