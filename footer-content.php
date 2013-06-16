a7a374afd07208946227269eae7f0e84aa3dcc86
Ben Nuttall <ben@bennuttall.com>
2013-06-16 01:49:53 +0100

    Removed extra line break after twitter badges

--- footer-content.php
+++ footer-content.php
@@ -1,6 +1,6 @@
 <a href="https://twitter.com/pi_weekly" class="twitter-follow-button" data-show-count="false">Follow @pi_weekly</a><br />
 <a href="https://twitter.com/ben_nuttall" class="twitter-follow-button" data-show-count="false">Follow @ben_nuttall</a>
-<a href="https://twitter.com/ryanteck" class="twitter-follow-button" data-show-count="false">Follow @ryanteck</a><br /><br />
+<a href="https://twitter.com/ryanteck" class="twitter-follow-button" data-show-count="false">Follow @ryanteck</a><br />
 
 <script type="text/javascript" language="JavaScript" src="http://piweekly.us7.list-manage.com/subscriber-count?b=11&u=50a341f5-6390-4e2a-a315-b2ab55cb32f5&id=2141506785"></script><br /><br />
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
