d24022ef9a8a4613f88269cc46ba7f20e537d251
Ben Nuttall <ben@bennuttall.com>
2013-09-03 13:15:07 +0100

    Corrected RSS link

--- footer-content.php
+++ footer-content.php
@@ -5,7 +5,7 @@
 <script type="text/javascript" language="JavaScript" src="http://piweekly.us7.list-manage.com/subscriber-count?b=11&u=50a341f5-6390-4e2a-a315-b2ab55cb32f5&id=2141506785"></script><br /><br />
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 
-<a href="ihttp://us7.campaign-archive1.com/feed?u=a3e42d3ea4355ad45198b39ba&id=2141506785" title="Subscribe via RSS"><img src="<?php bloginfo('template_url'); ?>/images/rss.png" /></a><br />
+<a href="http://us7.campaign-archive1.com/feed?u=a3e42d3ea4355ad45198b39ba&id=2141506785" title="Subscribe via RSS"><img src="<?php bloginfo('template_url'); ?>/images/rss.png" /></a><br />
 
 piweekly.net is a community project by <a href="http://twitter.com/ben_nuttall">Ben Nuttall</a> and <a href="http://twitter.com/ryanteck">Ryan Walmsley</a>.<br />
 <a href="https://github.com/bennuttall/piweekly"><em>piweekly</em></a> Wordpress Theme by <a href="http://bennuttall.com/">Ben Nuttall</a> - source available on <a href="https://github.com/bennuttall/piweekly">github</a>.
