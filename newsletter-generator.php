bb9b0ddf9e41dcdb90403263b4f538dd3bea5bf7
Ben Nuttall <ben@bennuttall.com>
2013-10-03 20:54:23 +0100

    Added dropdown for past issues

--- newsletter-generator.php
+++ newsletter-generator.php
@@ -7,7 +7,7 @@ function get_domain_from_url($url) {
 }
 
 function get_date($date) {
-    $datetime = DateTime::createFromFormat('Ymd', $date);
+    $datetime = new DateTime($date);
     return $datetime->format('l j F Y');
 }
 
@@ -15,14 +15,32 @@ function strip_paragraphs($content) {
     return str_replace('<p>', '', str_replace('</p>', '', $content));
 }
 
-$issue = new WP_Query('posts_per_page=1');
-
-$issue->the_post();
+$selected_issue = $_POST['issue'];
+$query = $selected_issue ? "p={$selected_issue}" : "posts_per_page=1";
 
 ?>
 
-<form>
+<form method="post">
+<br />
+<select name="issue">
+<?php
+
+$issues = new WP_Query('posts_per_page=1000');
+while ($issues->have_posts()):
+    $issues->the_post();
+    $selected = $selected_issue == get_the_ID() ? ' selected' : '';
+    echo "<option value='" . get_the_ID() . "'{$selected}>#" . get_field('issue_number') . " - " . get_the_title() . "</option>";
+endwhile;
+
+?>
+</select><input type="submit" value="Go" style="width:60px;" /><br /><br />
 <textarea rows="20" cols="100">
-<?php include 'email-template.php'; ?>
+<?php
+
+$issue = new WP_Query($query);
+$issue->the_post();
+include 'email-template.php';
+
+?>
 </textarea/><br />
 </form>
