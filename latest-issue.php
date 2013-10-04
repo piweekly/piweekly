cb2ff7bddbbc1839401e326d2d6255f0ee903f5d
Ben Nuttall <ben@bennuttall.com>
2013-10-04 18:20:22 +0100

    Reworded 'latest issue' on homepage

--- latest-issue.php
+++ latest-issue.php
@@ -1,5 +1,5 @@
 <?php $latest_issue = get_latest_issue(); ?>
 
 <p class="latest-issue">
-    Check out our latest issue: <a href="<?php echo $latest_issue['mailchimp_url']; ?>" target="_blank">#<?php echo $latest_issue['issue_number']; ?> &mdash; <?php echo $latest_issue['title']; ?></a>
+    Check out our latest newsletter: <a href="<?php echo $latest_issue['mailchimp_url']; ?>" target="_blank">Issue #<?php echo $latest_issue['issue_number']; ?> &mdash; <?php echo $latest_issue['title']; ?></a>
 </p>
