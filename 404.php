5f2c8e6ca5072e6817d3cfc354cb6e9989e5a769
Ben Nuttall <ben@bennuttall.com>
2013-10-07 17:15:37 +0100

    Moved 404 to part

---
+++ 404.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+
+Error 404 - you're looking for Pi in all the wrong places
+
+<?php get_footer();
--- index.php
+++ index.php
@@ -1,5 +1,1 @@
-<?php get_header(); ?>
-
-Error 404 - you're looking for Pi in all the wrong places
-
-<?php get_footer();
+<?php get_template_part('404');
