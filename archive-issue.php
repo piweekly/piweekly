983c4abebb2e6a5f31c1349f9913730e4c0ccb6d
Ben Nuttall <ben@bennuttall.com>
2013-10-26 12:45:13 +0100

    Removed ACF date field, replaced with WP built-in publish date

--- archive-issue.php
+++ archive-issue.php
@@ -1,5 +1,5 @@
 <li class="archive-issue">
     <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a>
     <a href="<?php the_permalink(); ?>">Issue #<?php the_field('issue_number'); ?> &mdash; <?php the_title(); ?></a>
-    <div class="archive-issue-date"><?php echo pw_date_format(get_field('date')); ?></div>
+    <div class="archive-issue-date"><?php echo pw_date_format(get_the_date()); ?></div>
 </li>
