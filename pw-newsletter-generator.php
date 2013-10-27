9f200e4b88cecc315274736319205a347887a597
Ben Nuttall <ben@bennuttall.com>
2013-10-27 09:18:30 +0000

    Changed from ACF date to WP date

--- pw-newsletter-generator.php
+++ pw-newsletter-generator.php
@@ -19,15 +19,12 @@ function pw_generate_newsletter() {
     include 'newsletter-generator.php';
 }
 
+// Functions used in template
+
 function get_domain_from_url($url) {
     $parsed_url = parse_url($url);
     $domain = str_replace('www.', '', $parsed_url['host']);
     return $domain;
-}
-
-function get_date($date) {
-    $datetime = new DateTime($date);
-    return $datetime->format('l j F Y');
 }
 
 function strip_paragraphs($content) {
