85b31fbc438670af3f13c2b71d5734154cb386a4
Ben Nuttall <ben@bennuttall.com>
2013-12-11 12:03:59 +0000

    Add Google+ link as publisher

--- themes/piweekly/header.php
+++ themes/piweekly/header.php
@@ -15,6 +15,7 @@
     <link rel="icon" type="image/png" href="<?php bloginfo('template_url'); ?>/images/piweekly-64x64.png" />
     <link rel="profile" href="http://gmpg.org/xfn/11" />
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+    <link rel="publisher" href="https://plus.google.com/+PiweeklyNet" />
     <!--[if lt IE 9]>
     <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
     <![endif]-->
