fbfd9289c6f4b1ec23596a70bcebaf1d3f36ca9e
Ben Nuttall <ben@bennuttall.com>
2014-02-02 12:39:18 +0100

    Add search page with google custom search, fix #1

---
+++ themes/piweekly/page-search.php
@@ -0,0 +1,23 @@
+<?php
+
+get_header();
+
+the_post();
+
+?>
+
+<script>
+  (function() {
+    var cx = '014439275497510315703:lkkdltymjyg';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+        '//www.google.com/cse/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
+<gcse:search></gcse:search>
+
+<?php get_footer();
