4aba866c5b274cc76a24fbd8bb57f43f5ec13e1a
Ben Nuttall <ben@bennuttall.com>
2014-10-25 01:05:25 +0100

    Updated footer and removed currents and gittip icons

--- themes/piweekly/footer-content.php
+++ themes/piweekly/footer-content.php
@@ -1,13 +1,11 @@
-<script type="text/javascript" language="JavaScript" src="http://piweekly.us7.list-manage.com/subscriber-count?b=11&u=50a341f5-6390-4e2a-a315-b2ab55cb32f5&id=2141506785"></script>
+<script src="http://piweekly.us7.list-manage.com/subscriber-count?b=11&u=50a341f5-6390-4e2a-a315-b2ab55cb32f5&id=2141506785"></script>
 
 <div class="icons">
-    <a href="https://twitter.com/pi_weekly" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/twitter-32x32.png" alt="Twitter" title="Follow us on Twitter @pi_weekly" /></a>
-    <a href="https://plus.google.com/+PiweeklyNet" rel="publisher" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/google-plus-32x32.png" alt="Google+" title="Google+" /></a>
-    <a href="https://www.github.com/bennuttall/piweekly" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/github-32x32.png" alt="Github" title="Browse the Pi Weekly repository on Github" /></a>
-    <a href="https://www.gittip.com/piweekly" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/gittip-32x32.png" alt="Gittip" title="Support us on Gittip" /></a>
-    <a href="http://www.google.com/producer/editions/CAowrMinBw/piweekly" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/google-currents-32x32.png" alt="Google Currents" title="Subscribe via Google Currents" /></a>
-    <a href="<?php bloginfo('atom_url'); ?>" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/rss-32x32.png" alt="RSS" title="Subscribe via RSS" /></a>
+    <a href="https://twitter.com/pi_weekly"><img src="<?php bloginfo('template_url'); ?>/images/twitter-32x32.png" alt="Twitter" title="Follow us on Twitter @pi_weekly" /></a>
+    <a href="https://plus.google.com/+PiweeklyNet" rel="publisher"><img src="<?php bloginfo('template_url'); ?>/images/google-plus-32x32.png" alt="Google+" title="Google+" /></a>
+    <a href="https://www.github.com/piweekly/piweekly"><img src="<?php bloginfo('template_url'); ?>/images/github-32x32.png" alt="Github" title="Browse the Pi Weekly repository on Github" /></a>
+    <a href="<?php bloginfo('atom_url'); ?>"><img src="<?php bloginfo('template_url'); ?>/images/rss-32x32.png" alt="RSS" title="Subscribe via RSS" /></a>
 </div>
 
-Pi Weekly is a community project by <a href="http://twitter.com/ben_nuttall" target="_blank">Ben Nuttall</a> and <a href="http://twitter.com/ryanteck" target="_blank">Ryan Walmsley</a>.<br />
-<a href="https://github.com/bennuttall/piweekly" target="_blank"><em>piweekly</em></a> Wordpress Theme by <a href="http://bennuttall.com/" target="_blank">Ben Nuttall</a> - source available on <a href="https://github.com/bennuttall/piweekly" target="_blank">github</a>.
+Pi Weekly is a community project by <a href="http://twitter.com/ben_nuttall">Ben Nuttall</a>.<br />
+<a href="https://github.com/bennuttall/piweekly"><em>piweekly</em></a> Wordpress Theme by <a href="http://bennuttall.com/">Ben Nuttall</a> - source available on <a href="https://github.com/bennuttall/piweekly">github</a>.
--- themes/piweekly/single.php
+++ themes/piweekly/single.php
@@ -17,7 +17,7 @@ the_post();
 
     <?php if (has_post_thumbnail()): ?>
         <h2>Picture of the week</h2>
-        <a href="<?php the_field('picture_link'); ?>" target="_blank">
+        <a href="<?php the_field('picture_link'); ?>">
             <?php the_post_thumbnail('email'); ?>
         </a>
 
@@ -30,7 +30,7 @@ the_post();
         <ul>
         <?php while (has_sub_field('news')): ?>
             <li>
-                <a href="<?php the_sub_field('url'); ?>" target="_blank"><?php the_sub_field('title'); ?></a> (<?php echo get_domain_from_url(get_sub_field('url')); ?>)<br />
+                <a href="<?php the_sub_field('url'); ?>"><?php the_sub_field('title'); ?></a> (<?php echo get_domain_from_url(get_sub_field('url')); ?>)<br />
                 <?php the_sub_field('summary'); ?>
             </li>
         <?php endwhile; ?>
@@ -43,7 +43,7 @@ the_post();
         <ul>
         <?php while (has_sub_field('projects')): ?>
             <li>
-                <a href="<?php the_sub_field('url'); ?>" target="_blank"><?php the_sub_field('title'); ?></a> (<?php echo get_domain_from_url(get_sub_field('url')); ?>)<br />
+                <a href="<?php the_sub_field('url'); ?>"><?php the_sub_field('title'); ?></a> (<?php echo get_domain_from_url(get_sub_field('url')); ?>)<br />
                 <?php the_sub_field('summary'); ?>
             </li>
         <?php endwhile; ?>
@@ -56,7 +56,7 @@ the_post();
         <ul>
         <?php while (has_sub_field('articles')): ?>
             <li>
-                <a href="<?php the_sub_field('url'); ?>" target="_blank"><?php the_sub_field('title'); ?></a> (<?php echo get_domain_from_url(get_sub_field('url')); ?>)<br />
+                <a href="<?php the_sub_field('url'); ?>"><?php the_sub_field('title'); ?></a> (<?php echo get_domain_from_url(get_sub_field('url')); ?>)<br />
                 <?php the_sub_field('summary'); ?>
             </li>
         <?php endwhile; ?>
@@ -70,11 +70,11 @@ the_post();
         if ($interview): ?>
             <hr />
             <h2>Interview: <?php echo get_the_title($interview); ?></h2>
-            <a href="<?php echo get_permalink($interview); ?>" target="_blank">
+            <a href="<?php echo get_permalink($interview); ?>">
                 <?php echo get_the_post_thumbnail($interview, 'email'); ?>
             </a>
             <?php echo get_field('intro', $interview); ?>
-            <a href="<?php echo get_permalink($interview); ?>" target="_blank">Read the full interview</a>
+            <a href="<?php echo get_permalink($interview); ?>">Read the full interview</a>
     <?php endif;
     endif;
 
@@ -84,7 +84,7 @@ the_post();
         <ul>
         <?php while (has_sub_field('events')): ?>
             <li>
-                <a href="<?php the_sub_field('url'); ?>" target="_blank"><?php the_sub_field('title'); ?></a> (<?php echo pw_event_date(get_sub_field('start_date'), get_sub_field('end_date')); ?>)
+                <a href="<?php the_sub_field('url'); ?>"><?php the_sub_field('title'); ?></a> (<?php echo pw_event_date(get_sub_field('start_date'), get_sub_field('end_date')); ?>)
             </li>
         <?php endwhile; ?>
         </ul>
@@ -93,7 +93,7 @@ the_post();
     if (get_field('sponsor')): ?>
         <hr />
         <h2>Thanks to our sponsor</h2>
-        <a href="<?php the_field('sponsor_url'); ?>" target="_blank">
+        <a href="<?php the_field('sponsor_url'); ?>">
             <img src="<?php $logo = get_field('sponsor_logo'); echo $logo['sizes']['email']; ?>" />
         </a>
         <?php the_field('sponsor_text');
@@ -101,11 +101,10 @@ the_post();
 
     <hr />
     <h2>Contact &amp; Submissions</h2>
-    <p>This newsletter is curated by <a href="http://twitter.com/ben_nuttall" target="_blank">@ben_nuttall</a> and <a href="http://twitter.com/ryanteck" target="_blank">@ryanteck</a>. Tweet links to <a href="http://twitter.com/pi_weekly" target="_blank">@pi_weekly</a> or email <a href="mailto:submissions@piweekly.net" target="_blank">submissions@piweekly.net</a></p>
+    <p>This newsletter is curated by <a href="http://twitter.com/ben_nuttall">@ben_nuttall</a>. Tweet links to <a href="http://twitter.com/pi_weekly">@pi_weekly</a> or email submissions [at] piweekly.net</p>
     <span class="support">
-        <a href="/sponsorship/">Sponsor Pi Weekly</a> or <a href="https://www.gittip.com/piweekly" target="_blank">support us on gittip</a>
+        <a href="/sponsorship/">Why not sponsor Pi Weekly?</a>
     </span>
-    <a href="https://www.gittip.com/piweekly" target="_blank" class="gittip-support">&gt;3</a>
     <div style="clear: both;"></div>
 
     <hr class="thick" />
